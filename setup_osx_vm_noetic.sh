4dc098a47e870396f25b176de714560466671dd1
HB's Macbook Pro <hb_pen@naver.com>
2023-10-27 16:12:24 +0900

    ROS VM

--- setup_osx_vm_noetic.sh
+++ setup_osx_vm_noetic.sh
@@ -1,7 +1,19 @@
+# HOW TO USE:
+# After clean install of Ubuntu 20.04 Server on VMWare Fusion running on MacOSX,
+# cd && mkdir Git && cd Git
+# git clone https://github.com/hyeonbeenlee/MyDockerfile.git 
+# cd MyDockerfile && bash setup_osx_vm_noetic.sh
+# sudo reboot
+# Then, connect to Remote Desktop to {VM's internal IP}:3389
+
 # Authorize
+# Modify "hyeonbeen" to your username if needed.
+# Modify "hyeonbeen" to your username if needed.
 sudo usermod -aG sudo hyeonbeen
 sudo -- sh -c "echo "hyeonbeen" | sudo -S chmod 777 /home"
 sudo -- sh -c "echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
+# Modify "hyeonbeen" to your username if needed.
+# Modify "hyeonbeen" to your username if needed.
 
 # Install apps
 sudo sed -i 's/archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
@@ -18,11 +30,12 @@ sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh
 sudo service sshd restart
 
 # Install XRDP
-sudo DEBIAN_FRONTEND=noninteractive apt-get install xrdp xfce4 xfce4-session xfce4-terminal -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install xrdp xfce4 xfce4-session terminator -y
 sudo sed -i 's/#AllowRootLogin=false/AllowRootLogin=true/' /etc/xrdp/sesman.ini
 sudo systemctl enable xrdp
 sudo adduser xrdp ssl-cert
-sudo -- sh -c "echo xfce4-session >~/.xsession"
+echo xfce4-session >~/.xsession
+sudo sed -i 's/exec \/bin\/sh \/etc\/X11\/Xsession//g' /etc/xrdp/startwm.sh
 sudo -- sh -c "echo unset DBUS_SESSION_BUS_ADDRESS >>/etc/xrdp/startwm.sh"
 sudo -- sh -c "echo unset XDG_RUNTIME_DIR >>/etc/xrdp/startwm.sh"
 sudo -- sh -c "echo . $HOME/.profile >>/etc/xrdp/startwm.sh"
@@ -44,13 +57,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt install apt-transport-https -y
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install code -y
 
-# Install miniconda
-# mkdir -p ~/miniconda3
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O ~/miniconda3/miniconda.sh
-# bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-# rm -rf ~/miniconda3/miniconda.sh
-# ~/miniconda3/bin/conda init bash
-
 # ROS-Noetic installation
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
@@ -65,16 +71,22 @@ rosdep update
 echo "alias cm="catkin_make"" >> ~/.bashrc
 # ROS Turtlebot3
 sudo apt-get install ros-noetic-joy ros-noetic-teleop-twist-joy -y
-sudo apt-get install ros-noetic-dynamixel-sdk -y
-sudo apt-get install ros-noetic-turtlebot3-msgs -y
-sudo apt-get install ros-noetic-turtlebot3 -y
-echo "export TURTLEBOT3_MODEL=waffle_pi" >> ~/.bashrc
-# Setup catkin_ws
 mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/src
-catkin_make
+cd ~/catkin_ws/src/
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/DynamixelSDK.git
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
+cd ~/catkin_ws && catkin_make
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "export TURTLEBOT3_MODEL=waffle_pi" >> ~/.bashrc
 
+# Install miniconda
+# mkdir -p ~/miniconda3
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O ~/miniconda3/miniconda.sh
+# bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+# rm -rf ~/miniconda3/miniconda.sh
+# ~/miniconda3/bin/conda init bash
 
 # Clean
 sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
