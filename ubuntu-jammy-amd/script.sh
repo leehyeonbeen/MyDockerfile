e5f90b157353acd9986842c1f83694d8e87bee35
Edward Hyeonbeen Lee <edward.hyeonbeen.lee@gmail.com>
2025-07-05 01:55:58 +0900

    delete cuda installation

--- ubuntu-jammy-amd/script.sh
+++ ubuntu-jammy-amd/script.sh
@@ -29,15 +29,6 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 
-# Install CUDA Toolkit 12.8
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda-repo-ubuntu2204-12-8-local_12.8.0-570.86.10-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-12-8-local_12.8.0-570.86.10-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2204-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-8 -y
-
 # Delete temp files
 apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
