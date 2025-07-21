dff71f579a21417977ff77e0c8881ef5695efd68
Hyeonbeen Lee <edward.hyeonbeen.lee@gmail.com>
2025-07-21 11:35:53 +0900

    fix: reorganize script to apply Korean mirrors and set timezone before application installation

--- initiate_isp_1.sh
+++ initiate_isp_1.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Apply Korean mirrors
+sudo sed -i 's/archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo sed -i 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo sed -i 's/ports.ubuntu.com/ftp.lanet.kr/g' /etc/apt/sources.list
+sudo apt update && sudo apt upgrade -y
+
+# Set timezone and sync
+sudo apt install ntpdate -y
+sudo ntpdate time.google.com
+sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+
 # Delete venv
 sudo rm -rf .venv/ /home/jovyan_venv/ # delete venv directories
 sed -i '/\.venv/d' ~/.bashrc # delete relevant lines in ~/.bashrc
@@ -9,25 +20,17 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ~/Miniconda3-latest-Linux-x86_64.sh
 rm -rf ~/Miniconda3-latest-Linux-x86_64.sh
 
-# Apply Korean mirrors
-sudo sed -i 's/archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
-sudo sed -i 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
-sudo sed -i 's/ports.ubuntu.com/ftp.lanet.kr/g' /etc/apt/sources.list
-sudo apt update && sudo apt upgrade -y
-
 # Install applications
-sudo apt install ca-certificates curl wget vim git tmux net-tools openssh-server rsync -y
+sudo apt install curl wget vim git tmux net-tools openssh-server rsync gzip unzip bash-completion -y
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 git config --global user.name "Edward Hyeonbeen Lee"
 git config --global user.email "edward.hyeonbeen.lee@gmail.com"
-git config pull.rebase false
+echo 'export GZIP="-9"' >> ~/.bashrc
 
-# Set timezone and sync
-sudo apt install ntpdate -y
-sudo ntpdate time.google.com
-sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
-
-# Setup tmux
+# tmux auto-completion
+curl -fSsL "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux" > ~/.bash.tmux-bash-completion
+echo 'source ~/.bash.tmux-bash-completion' >> ~/.bashrc
+# tmux config
 echo "set -g mouse on" >> ~/.tmux.conf
 echo "set -g history-limit 1000000" >> ~/.tmux.conf
 echo "set -g @scroll-speed-num-lines-per-scroll 1" >> ~/.tmux.conf
